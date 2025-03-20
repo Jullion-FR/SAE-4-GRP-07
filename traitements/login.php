64c7e754473c941e237ba4488f8a5ea091070f91
ruffaultravenelg <gemino.ruffaultravenel@gmail.com>
2025-03-20 18:35:34 +0100

    Fixed login bug no redirect

--- traitements/login.php
+++ traitements/login.php
@@ -50,7 +50,12 @@ if (!empty($result) && (isset($result[0]["1"]) && $result[0]["1"] == 1 || isset(
     $_SESSION["isAdmin"] = $isAdmin[0]['count'] > 0;
     $_SESSION['erreur'] = '';
 
-    header('Location: ' . $_POST['redirect']);
+    $redirect = $_POST['redirect'];
+    if (empty($redirect)) {
+        $redirect = "../index.php";
+    }
+
+    header('Location: ' . $redirect);
     exit;
 
 } else {
